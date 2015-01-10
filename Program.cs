1eac09ef60ed8bbe1bc6d78c13d958f10522a470
Bibi <bbyaneva@gmail.com>
2015-01-10 15:12:03 +0200

    Teleric Academy HOmework

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Problem 7. Print First and Last Name Create console application that prints your first and last name, each at a separate line.
+
+using System;
+
+
+namespace PrintMyNameTwoLines
+{
+    class PrintMynameTwolines
+    {
+        static void Main()
+        {
+            Console.WriteLine("Bibi");
+            Console.WriteLine("Yaneva");
+        }
+    }
+}
